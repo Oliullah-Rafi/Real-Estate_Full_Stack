ce2990b49e4f34d41566328c1a41e7ae9a0d83a0
Oliullah-Rafi <oliullahrafi@gmail.com>
2024-09-08 11:27:52 +0600

    Pages & Routes Creation completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
+import About from './pages/About';
+import Profile from './pages/Profile';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-blue-500">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/Sign-In' element={<SignIn/>}/>
+      <Route path='/Sign-Out' element={<SignOut/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+
+    </Routes>
+    
+    
+    </BrowserRouter>
   )
 }
